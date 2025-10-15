04b9f9fcf220ac6efa643dbc3bf777b66337cffb
ltai457 <ltai457@aucklanduni.ac.nz>
2025-10-15 15:03:29 +1300

    restart customer app

--- customer-app/src/App.jsx
+++ customer-app/src/App.jsx
@@ -1,24 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { CartProvider } from './contexts/CartContext'
-import TableSelectionPage from './pages/TableSelectionPage'
-import MenuPage from './pages/MenuPage'
-import CartPage from './pages/CartPage'
-import CheckoutPage from './pages/CheckoutPage'
-import OrderConfirmationPage from './pages/OrderConfirmationPage'
-
 function App() {
   return (
-    <BrowserRouter>
-      <CartProvider>
-        <Routes>
-          <Route path="/" element={<TableSelectionPage />} />
-          <Route path="/menu" element={<MenuPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/order-confirmation/:orderId" element={<OrderConfirmationPage />} />
-        </Routes>
-      </CartProvider>
-    </BrowserRouter>
+    <div>
+      <h1>Customer App - Fresh Start</h1>
+    </div>
   )
 }
 
